d81123b60890fbeb85f6a2008ff52905e8b6a392
JC Ton <tonjcl@gmail.com>
2022-12-03 14:15:43 +0100

    week2

---
+++ Week2/GiftList/server/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const verifyProof = require('../utils/verifyProof');
+const niceList = require('../utils/niceList');
+const MerkleTree = require('../utils/MerkleTree');
+
+const port = 1225;
+
+const app = express();
+app.use(express.json());
+
+// TODO: hardcode a merkle root here representing the whole nice list
+// create the merkle tree for the whole nice list
+const merkleTree = new MerkleTree(niceList);
+// paste the hex string in here, without the 0x prefix
+const MERKLE_ROOT = merkleTree.getRoot();
+
+
+app.post('/gift', (req, res) => {
+  // grab the parameters from the front-end here
+  const body = req.body;
+  const name = body['data']
+  // prove that a name is in the list 
+
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+  if (isInTheList) {
+    res.send(`${name} got a toy robot!`);
+  }
+  else {
+    res.send(`${name} are not on the list :(`);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
+});
